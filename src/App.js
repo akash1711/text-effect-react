659207a52990823405df2372d4d5084df5073149
akash1711 <nsakashu@gmail.com>
2021-07-04 10:24:47 +0530

    Text animation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <svg width="100vw" height="100vh">
+      <text x="45%" y="55%">Goutham</text>
+    </svg>
+  );
+}
+
+export default App;
