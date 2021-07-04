1ded7e473876e0917114cdd0f729bb19e312bc42
akash1711 <nsakashu@gmail.com>
2021-07-04 10:37:32 +0530

    Color Changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <svg width="100vw" height="100vh">
-      <text x="45%" y="55%">Goutham</text>
-    </svg>
-  );
+      <svg width="100vw" height="100vh">
+        <text x="45%" y="55%">Goutham</text>
+      </svg>
+    );
 }
 
 export default App;
